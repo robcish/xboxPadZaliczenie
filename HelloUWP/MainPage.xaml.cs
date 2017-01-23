8125dcd847a5b772e329fca66843cc5aba94e285
rrozanski <rrozanski@speednet.pl>
2017-01-23 10:41:44 +0100

    init

---
+++ HelloUWP/MainPage.xaml.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Linq;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.Gaming.Input;
+using Windows.UI.Popups;
+
+namespace HelloUWP
+{
+
+    public sealed partial class MainPage : Page
+    {
+        Gamepad controller;
+        public int scenarioCounter = 0;
+        public RotationClass rotation;
+        double randomRotation;
+        ContentDialog dialog;
+        DispatcherTimer dispatcherTimer;
+        TimeSpan period = TimeSpan.FromMilliseconds(100);
+
+
+        public MainPage()
+        {
+            Random random = new Random();
+            randomRotation = random.Next(5, 85);
+            rotation = new RotationClass(randomRotation);
+
+            this.InitializeComponent();
+            this.DataContext = rotation;
+            dispatcherTimer = new DispatcherTimer();
+            dispatcherTimer.Tick += dispatcherTimer_Tick;
+            dialog = new ContentDialog();
+            dialog.Content = "Kwadraty muszą być identycznie obrócone (Y = podpowiedź)";
+
+            dispatcherTimer.Start();
+        }
+
+        private void dispatcherTimer_Tick(object sender, object e)
+        {
+            if (Gamepad.Gamepads.Count > 0)
+            {
+                controller = Gamepad.Gamepads.First();
+                var reading = controller.GetCurrentReading();
+
+                if (reading.Buttons.HasFlag(GamepadButtons.Y))
+                {
+                    dialog.Hide();
+                    if (rotation.Rotation == rotation.RandomRotation)
+                    {
+                        textBlock1.Text = "Można przejść dalej";
+                    }
+                    else
+                    {
+                        textBlock1.Text = "Pomyłka: " + (rotation.Rotation - rotation.RandomRotation).ToString();
+                    }
+                }
+                else
+                {
+                    textBlock1.Text = "";
+                }
+
+            }
+
+        }
+
+        private async void button_Click(object sender, RoutedEventArgs e)
+        {
+            if (rotation.Rotation == rotation.RandomRotation)
+            {
+                this.Frame.Navigate(typeof(Page2));
+            }
+            else
+            {
+                try
+                {await dialog.ShowAsync();} catch{ dialog.Hide(); }
+            }
+        }
+
+    }
+
+    public class RotationClass
+    {
+        public RotationClass(double random)
+        {
+            this.randomRotationValue = random;
+        }
+
+        private double rotationValue;
+        public double Rotation
+        {
+            get { return rotationValue; }
+            set { rotationValue = value; }
+        }
+
+        private double randomRotationValue;
+        public double RandomRotation
+        {
+            get { return randomRotationValue; }
+            set { randomRotationValue = value; }
+        }
+    }
+}
---
+++ HelloUWP/Page3.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Linq;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.Gaming.Input;
+using Windows.UI.Popups;
+
+namespace HelloUWP
+{
+
+    public sealed partial class Page3 : Page
+    {
+        Gamepad controller;
+        DispatcherTimer dispatcherTimer;
+        TimeSpan period = TimeSpan.FromMilliseconds(100);
+        double xyz;
+
+        public Page3()
+        {
+
+            this.InitializeComponent();
+            dispatcherTimer = new DispatcherTimer();
+            dispatcherTimer.Tick += dispatcherTimer_Tick;
+            dispatcherTimer.Start();
+            textBlock.Visibility = Visibility.Collapsed;
+            textBlock2.Visibility = Visibility.Collapsed;
+        }
+
+        private void dispatcherTimer_Tick(object sender, object e)
+        {
+            if (Gamepad.Gamepads.Count > 0)
+            {
+                controller = Gamepad.Gamepads.First();
+                var reading = controller.GetCurrentReading();
+                if (reading.LeftTrigger > 0.1)
+                {
+                    double lt = Math.Round(reading.LeftTrigger, 1);
+                    textBlock.Text = lt.ToString();
+                }
+                else
+                {
+                    textBlock.Text = "0";
+                }
+
+                if (reading.RightTrigger > 0.1)
+                {
+                    double rt = Math.Round(reading.RightTrigger, 1);
+                    textBlock2.Text = rt.ToString();
+                }
+                else
+                {
+                    textBlock2.Text = "0";
+                }
+
+                if (reading.Buttons.HasFlag(GamepadButtons.Y))
+                {
+                    
+                }
+            }
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            Windows.ApplicationModel.Core.CoreApplication.Exit();
+        }
+    }
+
+}
